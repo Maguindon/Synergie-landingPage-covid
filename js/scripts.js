f2d0540719bbd1cac224ad1db9090a802cb68759
Jonathan Biard <1724744+jonathanbiard@users.noreply.github.com>
2020-05-20 23:08:01 -0400

    Debugged and fixed everything that needed fixing for the landing page's contact form to work with the php mailer

--- js/scripts.js
+++ js/scripts.js
@@ -3,96 +3,121 @@
     * Copyright 2013-2020 Start Bootstrap
     * Licensed under MIT (https://github.com/BlackrockDigital/startbootstrap-creative/blob/master/LICENSE)
     */
-    (function($) {
-  "use strict"; // Start of use strict
+(function($) {
+    "use strict"; // Start of use strict
 
-  // Smooth scrolling using jQuery easing
-  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: (target.offset().top - 72)
-        }, 1000, "easeInOutExpo");
-        return false;
-      }
-    }
-  });
+    // Smooth scrolling using jQuery easing
+    $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: (target.offset().top - 72)
+                }, 1000, "easeInOutExpo");
+              
+                return false;
+            }
+        }
+    });
 
-  // Closes responsive menu when a scroll trigger link is clicked
-  $('.js-scroll-trigger').click(function() {
-    $('.navbar-collapse').collapse('hide');
-  });
+    // Closes responsive menu when a scroll trigger link is clicked
+    $('.js-scroll-trigger').click(function() {
+        $('.navbar-collapse').collapse('hide');
+    });
 
-  // Activate scrollspy to add active class to navbar items on scroll
-  $('body').scrollspy({
-    target: '#mainNav',
-    offset: 75
-  });
+    // Activate scrollspy to add active class to navbar items on scroll
+    $('body').scrollspy({
+        target: '#mainNav',
+        offset: 75
+    });
 
-  // Collapse Navbar
-  var navbarCollapse = function() {
-    if ($("#mainNav").offset().top > 100) {
-      $("#mainNav").addClass("navbar-scrolled");
-      $("#logo-top").addClass("navbar-scrolled");
-    } else {
-      $("#mainNav").removeClass("navbar-scrolled");
-      $("#logo-top").removeClass("navbar-scrolled");
-    }
-  };
-  // Collapse now if page is not at top
-  navbarCollapse();
-  // Collapse the navbar when page is scrolled
-  $(window).scroll(navbarCollapse);
+    // Collapse Navbar
+    var navbarCollapse = function() {
+        if ($("#mainNav").offset().top > 100) {
+            $("#mainNav").addClass("navbar-scrolled");
+            $("#logo-top").addClass("navbar-scrolled");
+        } else {
+            $("#mainNav").removeClass("navbar-scrolled");
+            $("#logo-top").removeClass("navbar-scrolled");
+        }
+    };
 
-  // Magnific popup calls
-  $('#portfolio').magnificPopup({
-    delegate: 'a',
-    type: 'image',
-    tLoading: 'Loading image #%curr%...',
-    mainClass: 'mfp-img-mobile',
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1]
-    },
-    image: {
-      tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
-    }
-  });
+    // Collapse now if page is not at top
+    navbarCollapse();
+    // Collapse the navbar when page is scrolled
+    $(window).scroll(navbarCollapse);
 
+    // Magnific popup calls
+    $('#portfolio').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0, 1]
+        },
+        image: {
+            tError: '<a href="%url%">The image #%curr%</a> could not be loaded.'
+        }
+    });
 
+    $('#contact-button').on('click', function () {
+        var $status = $('#status')
+
+        var $name = $('#name');
+        if ($name.val() == "") {
+            $status.text("Name cannot be empty");
+            return false;
+        }
+
+        var $email = $('#email');
+        if ($email.val() == "") {
+            $status.text("Email cannot be empty");
+            return false;
+        } // let php's filter_var do the email validation
+
+        var $subject = $('#subject');
+        if ($subject.val() == "") {
+            $status.text("Subject cannot be empty");
+            return false;
+        }
+
+        var $message = $('#message');
+        if ($message.val() == "") {
+            $status.text("Message cannot be empty");
+            return false;
+        }
+
+        $status.text("Sending...")
+
+        var formData = {
+            name: $name.val(),
+            email: $email.val(),
+            subject: $subject.val(),
+            message: $message.val()
+        };
+
+        $.ajax({
+            url: "mail.php",
+            type: "POST",
+            data: formData,
+            success: function (data, textStatus, jqXHR) {
+                $status.text(data);
+
+                $subject.val("")
+                $message.val("")
+            },
+            error: function (jqXHR, textStatus, errorThrown) {
+                $status.text(textStatus);
+
+                console.log(textStatus)
+                console.log(errorThrown)
+            }
+        });
+    })
 })(jQuery); // End of use strict
-
-// Added to validate form
-function validateForm() {
-  var name =  document.getElementById('name').value;
-  if (name == "") {
-      document.querySelector('.status').innerHTML = "Name cannot be empty";
-      return false;
-  }
-  var email =  document.getElementById('email').value;
-  if (email == "") {
-      document.querySelector('.status').innerHTML = "Email cannot be empty";
-      return false;
-  } else {
-      var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if(!re.test(email)){
-          document.querySelector('.status').innerHTML = "Email format invalid";
-          return false;
-      }
-  }
-  var subject =  document.getElementById('subject').value;
-  if (subject == "") {
-      document.querySelector('.status').innerHTML = "Subject cannot be empty";
-      return false;
-  }
-  var message =  document.getElementById('message').value;
-  if (message == "") {
-      document.querySelector('.status').innerHTML = "Message cannot be empty";
-      return false;
-  }
-  document.querySelector('.status').innerHTML = "Sending...";
-}
 
--- mail.php
+++ mail.php
@@ -3,13 +3,13 @@ if(isset( $_POST['name'])) {
     $name = $_POST['name'];
 }
 if(isset( $_POST['email'])) {
-    $name = $_POST['email'];
+    $email = $_POST['email'];
 }
 if(isset( $_POST['message'])) {
-    $name = $_POST['message'];
+    $message = $_POST['message'];
 }
 if(isset( $_POST['subject'])) {
-    $name = $_POST['subject'];
+    $subject = $_POST['subject'];
 }
 
 if ($name === ''){
